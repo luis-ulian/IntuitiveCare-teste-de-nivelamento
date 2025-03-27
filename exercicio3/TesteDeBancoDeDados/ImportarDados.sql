403592556b96722d0ecddee267ffcd6cedfbe4f3
Luís Ulian <luissteam44@gmail.com>
2025-03-27 19:42:24 -0300

    exercise 3 almost complete

--- exercicio3/TesteDeBancoDeDados/ImportarDados.sql
+++ exercicio3/TesteDeBancoDeDados/ImportarDados.sql
@@ -1,6 +1,8 @@
 TRUNCATE TABLE dbo.Empresas;
 GO
 
+
+
 BULK INSERT dbo.Empresas
 FROM "C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\Relatorio_cadop.csv"
 WITH
@@ -18,98 +20,83 @@ WITH
 TRUNCATE TABLE dbo.Demonstracoes_Contabeis;
 GO
 
-BULK INSERT dbo.Demonstracoes_Contabeis
-FROM "C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\1T2023.csv"
-WITH
-(
-    FORMAT='CSV',
-    FIRSTROW = 2,
-    FIELDQUOTE='"',
-    FIELDTERMINATOR = ';',
-    ROWTERMINATOR = '\n',
-    CODEPAGE='65001'
+CREATE TABLE ##tmp(
+    [DTA] DATETIME,
+    [COD_REGISTRO_ANS] INT NOT NULL,
+    [CD_CONTA_CONTABIL] VARCHAR(15),
+    [DESCRICAO] VARCHAR(255),
+    [VLR_SALDO_INICIAL] VARCHAR(30),
+    [VLR_SALDO_FINAL] VARCHAR(30)
 )
 
-BULK INSERT dbo.Demonstracoes_Contabeis
-FROM "C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\1T2024.csv"
-WITH
-(
-    FORMAT='CSV',
-    FIRSTROW = 2,
-    FIELDQUOTE='"',
-    FIELDTERMINATOR = ';',
-    ROWTERMINATOR = '\n',
-    CODEPAGE='65001'
-)
+GO
 
-BULK INSERT dbo.Demonstracoes_Contabeis
-FROM "C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\2T2023.csv"
-WITH
-(
-    FORMAT='CSV',
-    FIRSTROW = 2,
-    FIELDQUOTE='"',
-    FIELDTERMINATOR = ';',
-    ROWTERMINATOR = '\n',
-    CODEPAGE='65001'
-)
+CREATE FUNCTION InserirBulk(@path NVARCHAR(1000)) 
+RETURNS NVARCHAR(1000)
+AS
+    BEGIN
+        declare @func NVARCHAR(1000)
+        set @func = N'BULK INSERT ##tmp
+        FROM '''+@path+'''
+        WITH
+        (
+            FORMAT=''CSV'',
+            FIRSTROW = 2,
+            FIELDQUOTE=''"'',
+            FIELDTERMINATOR = '';'',
+            ROWTERMINATOR = ''\n'',
+            CODEPAGE=''65001''
+        )'
+        
+        RETURN @func
+    END
+GO
+DECLARE @path1 NVARCHAR(100) = 'C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\1T2023.csv'
+declare @sql NVARCHAR(1000) = (SELECT dbo.InserirBulk(@path1))
 
-BULK INSERT dbo.Demonstracoes_Contabeis
-FROM "C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\2T2024.csv"
-WITH
-(
-    FORMAT='CSV',
-    FIRSTROW = 2,
-    FIELDQUOTE='"',
-    FIELDTERMINATOR = ';',
-    ROWTERMINATOR = '\n',
-    CODEPAGE='65001'
-)
+EXEC master.sys.sp_executesql @sql
 
-BULK INSERT dbo.Demonstracoes_Contabeis
-FROM "C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\3T2023.csv"
-WITH
-(
-    FORMAT='CSV',
-    FIRSTROW = 2,
-    FIELDQUOTE='"',
-    FIELDTERMINATOR = ';',
-    ROWTERMINATOR = '\n',
-    CODEPAGE='65001'
-)
+INSERT INTO dbo.Demonstracoes_Contabeis SELECT t.DTA,
+                                                t.COD_REGISTRO_ANS,
+                                                t.CD_CONTA_CONTABIL,
+                                                t.DESCRICAO,
+                                                CAST(REPLACE(trim(t.VLR_SALDO_INICIAL), ',', '.') AS MONEY),
+                                                CAST(REPLACE(trim(t.VLR_SALDO_FINAL), ',', '.') AS MONEY)
+                                                   FROM ##tmp t
 
-BULK INSERT dbo.Demonstracoes_Contabeis
-FROM "C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\3T2024.csv"
-WITH
-(
-    FORMAT='CSV',
-    FIRSTROW = 2,
-    FIELDQUOTE='"',
-    FIELDTERMINATOR = ';',
-    ROWTERMINATOR = '\n',
-    CODEPAGE='65001'
-)
+drop table ##tmp
 
-BULK INSERT dbo.Demonstracoes_Contabeis
-FROM "C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\4T2023.csv"
-WITH
-(
-    FORMAT='CSV',
-    FIRSTROW = 2,
-    FIELDQUOTE='"',
-    FIELDTERMINATOR = ';',
-    ROWTERMINATOR = '\n',
-    CODEPAGE='65001'
-)
+drop function dbo.InserirBulk
 
-BULK INSERT dbo.Demonstracoes_Contabeis
-FROM "C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\4T2024.csv"
-WITH
-(
-    FORMAT='CSV',
-    FIRSTROW = 2,
-    FIELDQUOTE='"',
-    FIELDTERMINATOR = ';',
-    ROWTERMINATOR = '\n',
-    CODEPAGE='65001'
-)
+SELECT * from dbo.Demonstracoes_Contabeis
+
+"C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\1T2024.csv"
+
+
+"C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\2T2023.csv"
+
+
+"C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\2T2024.csv"
+
+
+"C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\3T2023.csv"
+
+
+"C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\3T2024.csv"
+
+
+"C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\4T2023.csv"
+
+"C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\4T2024.csv"
+
+
+INSERT INTO dbo.Demonstracoes_Contabeis
+SELECT a.*, CAST(REPLACE(VLR_SALDO_INICIAL, ',', '.') AS DECIMAL(10, 2)), 
+            CAST(REPLACE(VLR_SALDO_FINAL, ',', '.') AS DECIMAL(10, 2))
+FROM OPENROWSET( BULK 'C:\programação\AplicaçõesDeVagas\IntuitiveCare\exercicio3\arquivos\4T2024.csv' ,
+                FORMAT='CSV',
+                FIRSTROW = 2,
+                FIELDQUOTE='"',
+                CODEPAGE='65001',
+                FORMATFILE=''
+) as a;
--- exercicio3/TesteDeBancoDeDados/LerTabela.sql
+++ exercicio3/TesteDeBancoDeDados/LerTabela.sql
@@ -1,2 +1,33 @@
-SELECT * FROM dbo.Empresas
-SELECT * FROM dbo.Demonstracoes_Contabeis
+WITH UltimoTrimestre AS (
+    SELECT 
+        MAX(DTA) AS DataMaxima
+    FROM 
+        Demonstracoes_Contabeis
+),
+Filtradas AS (
+    SELECT 
+        dc.COD_REGISTRO_ANS,
+        e.DES_RAZAO,
+        dc.VLR_SALDO_FINAL,
+        dc.VLR_SALDO_INICIAL
+    FROM 
+        Demonstracoes_Contabeis dc
+    INNER JOIN 
+        Empresas e ON dc.COD_REGISTRO_ANS = e.COD_REGISTRO_ANS
+    WHERE 
+        DC.DESCRICAO = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR'
+        AND dc.DTA >= DATEADD(QUARTER, -1, (SELECT DataMaxima FROM UltimoTrimestre))
+)
+SELECT 
+    TOP 10 
+    COD_REGISTRO_ANS,
+    DES_RAZAO,
+    SUM(VLR_SALDO_FINAL - VLR_SALDO_INICIAL) AS TotalDespesas
+FROM 
+    Filtradas
+WHERE 
+    VLR_SALDO_FINAL < VLR_SALDO_INICIAL
+GROUP BY 
+    COD_REGISTRO_ANS, DES_RAZAO
+ORDER BY 
+    TotalDespesas ASC;
